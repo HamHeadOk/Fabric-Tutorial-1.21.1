3ca101e17667f0886c2caaada1b201cd89037d0a
HamHeadOk <hamheadtf2@gmail.com>
2025-02-20 23:41:10 -0500

    awd

--- src/main/java/net/ham/tutorialmod/TutorialMod.java
+++ src/main/java/net/ham/tutorialmod/TutorialMod.java
@@ -7,13 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorialmod";
-
-
+		public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		ModItems.registerModItems();
-	}
+		@Override
+		public void onInitialize() {
+			ModItems.registerModItems();
+		}
 }
