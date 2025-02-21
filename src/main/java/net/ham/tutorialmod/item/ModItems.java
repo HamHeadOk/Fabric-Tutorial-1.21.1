944325cbe07e8e1f393e1185c4e555f7942842a6
HamHeadOk <hamheadtf2@gmail.com>
2025-02-20 22:43:23 -0500

    awd

---
+++ src/main/java/net/ham/tutorialmod/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.ham.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.ham.tutorialmod.TutorialMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item WEED = registerItem("weed", new Item(new Item.Settings()));
+    //public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(WEED);
+            //entries.add(RAW_PINK_GARNET);
+        });
+    }
+}
